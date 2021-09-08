ae052b8549b27c8dd1d19f4fa18212315c1847dd
RH <rh@example.com>
2021-09-08 07:05:33 +0100

    Added a 'getm' modifier (to complement 'setm')

---
+++ lib/mcex/modifier/getm.ex
@@ -0,0 +1,27 @@
+defmodule Mcex.Modifier.Getm do
+  use Mc.Railway, [:modify]
+
+  def modify(buffer, args) do
+    getm(buffer, args)
+  end
+
+  def getm(buffer, ""), do: getm_(buffer, "\n---\n")
+  def getm(buffer, separator), do: getm_(buffer, separator)
+
+  def getm_(buffer, separator) do
+    case Mc.Util.InlineString.uri_decode(separator) do
+      {:ok, decoded_separator} ->
+        result =
+          buffer
+          |> String.split()
+          |> Enum.map(fn key -> {key, Mc.modify("", "get #{key}")} end)
+          |> Enum.map(fn {key, {:ok, value}} -> "#{key}\n#{value}" end)
+          |> Enum.join(decoded_separator)
+
+        {:ok, result}
+
+      _error ->
+        usage(:modify, "[<uri encoded separator>]")
+    end
+  end
+end
