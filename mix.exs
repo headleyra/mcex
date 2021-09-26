30b4a10878c3244ab648caaacbc04192ed4f6f3d
RH <rh@example.com>
2021-09-26 06:57:34 +0100

    Removed redundant dependencies :jason and :httpoison (both are provided by :mc)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.9.0",
+      version: "0.9.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +21,6 @@ defmodule Mcex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.6"},
-      {:jason, "~> 1.2"},
       {:elixir_uuid, "~> 1.2"},
       {:tesla, "~> 1.3"},
 
