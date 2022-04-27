b896e41bff804fefa10f4c4d4d16a70348f192b9
RH <rh@example.com>
2022-04-27 17:47:59 +0100

    Elixir >= 1.3.x is now mandatory (to accommodate the upgrade of the :mc dependency)

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.13.0",
-      elixir: "~> 1.9",
+      version: "0.14.0",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
