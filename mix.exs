76a7cd827b75bb11acbfbe955ea83a06377bb742
RH <rh@example.com>
2024-01-11 19:35:20 +0000

    Elixir >= 1.15 is now mandatory

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.47.0",
-      elixir: "~> 1.13",
+      version: "0.48.0",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
