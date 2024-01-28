a087d326a210e0f5d627ce6abfe0752b05cddbdf
RH <rh@example.com>
2024-01-28 03:40:44 +0000

    Update :mc dependency (0.75.0)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.48.0",
+      version: "0.49.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
