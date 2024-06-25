17adb88ffb57bd02d9aabbf501da6c2f356b20b9
RH <rh@example.com>
2024-06-25 21:46:59 +0100

    Add a 'setm' file load utility function

---
+++ lib/mcex.ex
@@ -0,0 +1,20 @@
+defmodule Mcex do
+  @glob "*.setm.txt"
+  @setm_modifier "setm"
+
+  def setm(dir, mappings) do
+    Path.join(dir, @glob)
+    |> Path.wildcard()
+    |> Enum.map(fn file -> {Path.basename(file), File.read!(file)} end)
+    |> Enum.map(fn {file, setm} -> {file, Mc.modify(setm, @setm_modifier, mappings)} end)
+    |> Enum.map(fn file_result -> summarize(file_result) end)
+  end
+
+  def summarize({file, {:ok, _}}) do
+    {file, :ok}
+  end
+
+  def summarize({file, {:error, _}}) do
+    {file, :error}
+  end
+end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.62.4",
+      version: "0.63.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
