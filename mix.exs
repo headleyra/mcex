e4a41bc5d045e2fa4a133660d57f62828238de77
RH <rh@example.com>
2024-12-14 06:17:34 +0000

    Remove redundant dependency (tesla)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.71.0",
+      version: "0.71.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -22,7 +22,6 @@ defmodule Mcex.MixProject do
   defp deps do
     [
       {:elixir_uuid, "~> 1.2"},
-      {:tesla, "~> 1.3"},
 
       # Mc
       {:mc, git: "https://github.com/headleyra/mc.git"},
