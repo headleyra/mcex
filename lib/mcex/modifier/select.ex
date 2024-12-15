12d766b08cf2842b98b2b4c2e3484ba308205d96
RH <rh@example.com>
2024-12-15 11:00:53 +0000

    Refactor 'select' modifier: move non-public interface functions into a new module

--- lib/mcex/modifier/select.ex
+++ lib/mcex/modifier/select.ex
@@ -3,7 +3,7 @@ defmodule Mcex.Modifier.Select do
 
   def modify(buffer, args, _mappings) do
     buffer_lines = String.split(buffer, "\n")
-    line_specs = line_specs_from(args)
+    line_specs = Mcex.Select.line_specs(args)
 
     if Enum.any?(line_specs, fn line_spec -> line_spec == :error end) do
       oops("bad line spec(s)")
@@ -15,41 +15,6 @@ defmodule Mcex.Modifier.Select do
         |> Enum.reverse()
         |> Enum.join("\n")
       }
-    end
-  end
-
-  defp line_specs_from(args) do
-    String.split(args, ",")
-    |> Enum.map(fn line_spec -> parse(line_spec) end)
-  end
-
-  def parse(line_spec) do
-    case String.split(line_spec, "-", parts: 2) do
-      [int_str] ->
-        int_from(int_str)
-
-      [int_str_a, int_str_b] ->
-        int_from(int_str_a, int_str_b)
-    end
-  end
-
-  def int_from(int_str) do
-    case Mc.String.to_int(int_str) do
-      {:ok, int} when int > 0 ->
-        int - 1
-
-      _error ->
-        :error
-    end
-  end
-
-  def int_from(int_str_a, int_str_b) do
-    with \
-      first when is_integer(first) <- int_from(int_str_a),
-      last when is_integer(last) <- int_from(int_str_b)
-    do
-      first..last
-      |> Enum.to_list()
     end
   end
 end
