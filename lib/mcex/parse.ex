85f8cb94ddb14adda56b3c3a2c86276212c34ead
RH <rh@example.com>
2025-02-10 07:45:37 +0000

    Refactor the Mcex.Parse module

--- lib/mcex/parse.ex
+++ lib/mcex/parse.ex
@@ -1,64 +1,19 @@
 defmodule Mcex.Parse do
   def mapify(string) do
-    args_list =
-      string
-      |> String.trim()
-      |> String.split(~r/\s+(?=\w+:)/)
+    string
+    |> String.split()
+    |> Enum.reduce_while(%{}, fn kv_pair, acc -> map(kv_pair, acc) end)
+  end
 
-    case args_list do
-      [""] ->
-        %{}
+  defp map(kv_pair, acc) do
+    case String.split(kv_pair, ":", parts: 2) do
+      [key, value] ->
+        key_atom = String.to_atom(key)
+        value_decoded = URI.decode(value)
+        {:cont, Map.put(acc, key_atom, value_decoded)}
 
-      non_emtpy_args_list ->
-        mapize(non_emtpy_args_list)
+      _parse_error ->
+        {:halt, :error}
     end
-  end
-
-  def split(string) do
-    parse_list =
-      string
-      |> String.trim_leading()
-      |> String.split(" ", parts: 2)
-
-    case parse_list do
-      [separator, parts] ->
-        uri_separator = URI.decode(separator)
-        String.split(parts, uri_separator)
-
-      [_separator] ->
-        [""]
-    end
-  end
-
-  defp mapize(args_list) do 
-    map =
-      args_list
-      |> Enum.map(fn arg_str -> split_arg(arg_str) end)
-      |> Enum.into(%{}, fn [k, v] -> encode(k, v) end)
-
-    if valid(map), do: map, else: :error
-  end
-
-  defp valid(map) do
-    map
-    |> Map.values()
-    |> Enum.any?(fn value -> value != :error end)
-  end
-
-  defp split_arg(arg_str) do
-    case String.split(arg_str, ":") do
-      [k, v] ->
-        [k, v]
-
-      _error ->
-        [:error, nil]
-    end
-  end
-
-  defp encode(:error, _value), do: {"", :error}
-  defp encode(key, value) do
-    key_atom = String.to_atom(key)
-    value_uri = URI.decode(value)
-    {key_atom, value_uri}
   end
 end
--- test/mcex/modifier/if_test.exs
+++ test/mcex/modifier/if_test.exs
@@ -11,9 +11,9 @@ defmodule Mcex.Modifier.IfTest do
     test "runs <true script> if <regex> matches `buffer`, else runs <false script>", do: true
 
     test "works", %{mappings: mappings} do
-      assert If.modify("foo", "r:fo t:b true dat f:b nah!", mappings) == {:ok, "true dat"}
-      assert If.modify("more stuff", "t:b yep  r:ff$  f:b nah", mappings) == {:ok, "yep"}
-      assert If.modify("howdy", "r:h.wdi  f:range 3  t:date", mappings) == {:ok, "1\n2\n3"}
+      assert If.modify("foo", "r:fo t:b%20true%20dat f:b%20nah!", mappings) == {:ok, "true dat"}
+      assert If.modify("more stuff", "t:b%20yep  r:ff$  f:b%20nah", mappings) == {:ok, "yep"}
+      assert If.modify("howdy", "r:h.wdi  f:range%203  t:date", mappings) == {:ok, "1\n2\n3"}
     end
 
     @parse_err "Mcex.Modifier.If: parse error"
@@ -31,8 +31,8 @@ defmodule Mcex.Modifier.IfTest do
     end
 
     test "works with ok tuples", %{mappings: mappings} do
-      assert If.modify({:ok, "aaa"}, "r:.aa  t:b t  f:b f", mappings) == {:ok, "t"}
-      assert If.modify({:ok, "aaa"}, "r:ba.  t:b t  f:b f", mappings) == {:ok, "f"}
+      assert If.modify({:ok, "aaa"}, "r:.aa  t:b%20t  f:b%20f", mappings) == {:ok, "t"}
+      assert If.modify({:ok, "aaa"}, "r:ba.  t:b%20t  f:b%20f", mappings) == {:ok, "f"}
     end
 
     test "allows error tuples to pass through" do
