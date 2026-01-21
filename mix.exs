17ed99f2a686b7d51f1c9d91efb627910fe22a20
RH <rh@example.com>
2026-01-21 20:02:05 +0000

    Delete the (now) redundant Postgres DB modifiers, 'cache' and 'cacheclear'
    Upgrade:
    castore 1.0.14 => 1.0.17
    postgrex 0.21.1 => 0.22.0 (minor)
    redix 1.5.2 => 1.5.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mcex.MixProject do
   def project do
     [
       app: :mcex,
-      version: "0.96.0",
+      version: "0.97.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps()
