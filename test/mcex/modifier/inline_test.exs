77845e3dff62b522b25bfbb3585f1c93e2c18fd4
RH <rh@example.com>
2026-03-01 15:58:51 +0000

    Refactor the 'inline' modifier test

--- test/mcex/modifier/inline_test.exs
+++ test/mcex/modifier/inline_test.exs
@@ -2,37 +2,44 @@ defmodule Mcex.Modifier.InlineTest do
   use ExUnit.Case, async: true
   alias Mcex.Modifier.Inline
 
+  setup do
+    %{mappings: Mc.Mappings.s()}
+  end
+
   describe "modify/3" do
-    test "expands `buffer` as an 'inline string'" do
-      assert Inline.modify("just normal stuff", "", Mc.Mappings.s()) == {:ok, "just normal stuff"}
-      assert Inline.modify("will split into; lines", "", Mc.Mappings.s()) == {:ok, "will split into\nlines"}
-      assert Inline.modify("won't split into;lines", "", Mc.Mappings.s()) == {:ok, "won't split into;lines"}
-      assert Inline.modify("big; tune; ", "", Mc.Mappings.s()) == {:ok, "big\ntune\n"}
+    test "expands `buffer` as an 'inline string'", %{mappings: mappings} do
+      assert Inline.modify("just normal stuff", "", mappings) == {:ok, "just normal stuff"}
+      assert Inline.modify("will split into; lines", "", mappings) == {:ok, "will split into\nlines"}
+      assert Inline.modify("won't split into;lines", "", mappings) == {:ok, "won't split into;lines"}
+      assert Inline.modify("big; tune; ", "", mappings) == {:ok, "big\ntune\n"}
     end
 
-    test "runs back-ticked scripts in place" do
-      assert Inline.modify("zero {range 4} five", "", Mc.Mappings.s()) == {:ok, "zero 1\n2\n3\n4 five"}
-      assert Inline.modify("do you {buffer foo}?", "", Mc.Mappings.s()) == {:ok, "do you foo?"}
-      assert Inline.modify("yes {buffer WHEE; casel; replace whee we} can", "", Mc.Mappings.s()) == {:ok, "yes we can"}
-      assert Inline.modify("; ;tumble; weed; ", "", Mc.Mappings.s()) == {:ok, "\n;tumble\nweed\n"}
+    test "runs 'curly scripts' in place", %{mappings: mappings} do
+      assert Inline.modify("zero {range 4} five", "", mappings) == {:ok, "zero 1\n2\n3\n4 five"}
+      assert Inline.modify("do you {buffer foo}?", "", mappings) == {:ok, "do you foo?"}
+      assert Inline.modify("yes {buffer WHEE; casel; replace whee we} can", "", mappings) == {:ok, "yes we can"}
+      assert Inline.modify("; ;tumble; weed; ", "", mappings) == {:ok, "\n;tumble\nweed\n"}
     end
 
-    test "expands multiple 'inline strings'" do
-      assert Inline.modify("14da {buffer TREBLE; casel} 24da {buffer x; replace x bass}", "", Mc.Mappings.s()) ==
-        {:ok, "14da treble 24da bass"}
+    test "expands multiple 'inline strings'", %{mappings: mappings} do
+      buffer = "14da {buffer TREBLE; casel} 24da {buffer x; replace x bass}"
+      assert Inline.modify(buffer, "", mappings) == {:ok, "14da treble 24da bass"}
     end
 
-    test "handles 'inline strings' that return errors" do
-      assert Inline.modify("{error oops}", "", Mc.Mappings.s()) == {:error, "oops"}
-      assert Inline.modify("{error first} `error second`", "", Mc.Mappings.s()) == {:error, "first"}
+    test "returns errors", %{mappings: mappings} do
+      assert Inline.modify("{error oops}", "", mappings) == {:error, "oops"}
+    end
+
+    test "returns the first error", %{mappings: mappings} do
+      assert Inline.modify("{error first} {error second}", "", mappings) == {:error, "first"}
     end
 
     test "works with ok tuples" do
-      assert Inline.modify({:ok, "lock; down"}, "", Mc.Mappings.s()) == {:ok, "lock\ndown"}
+      assert Inline.modify({:ok, "lock; down"}, "", %{}) == {:ok, "lock\ndown"}
     end
 
     test "allows error tuples to pass through" do
-      assert Inline.modify({:error, "reason"}, "", Mc.Mappings.s()) == {:error, "reason"}
+      assert Inline.modify({:error, "reason"}, "", %{}) == {:error, "reason"}
     end
   end
 end
